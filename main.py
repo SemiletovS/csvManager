8fca50e027dce518ae26175416c8b8531f328c8c
Dmitri Miroshnichenko <djek2003djek@gmail.com>
2023-03-16 21:10:58 +0300

    ui/file

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print('csv Manager')
+import os
+import file
+import listfunk
+import UI
+
+
+
+def main():
+    os.system('cls')
+    ui.show_menu()
+    try:
+        choice = ui.choice_menu()
+        print(choice)
+        if choice==7:
+            print(file.read_fale('data.csv'))
+
+    except Exception as ex:
+        print(str(ex))
+        input()
+
+
+
+
+
+        main()
